04d747808d14af878542f5e9dec06a24681c8817
Probal Bose <probos22@gmail.com>
2024-11-22 11:56:58 +0000

    Adding python exercise notebooks from UCD

--- automate_boring_stuff/coinFlipStreak.py
+++ automate_boring_stuff/coinFlipStreak.py
@@ -1,8 +1,19 @@
 import random
 
-numberOfStreak = 0
+numberOfStreaks = 0
 
 for experimentNumber in range(10000):
-    # Code that creates a lists of 100 'heads' or 'tails' values.
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    flips = []
+    for i in range(101):
+        if random.randint(0, 1):
+            flips.append('H')
+        else:
+            flips.append('T')
 
+    print(''.join(flips))
+
+    # Code that checks if there is a streak of 6 heads or tails in a row.
     
+
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
